a5066f104b77f551a0ec2d053aa9faec12cc11fe
우성현 <wshyun314@naver.com>
2025-06-02 13:40:03 +0000

    Add project metadata files

--- setup.py
+++ setup.py
@@ -1,35 +1,39 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='nicewoo',
+    name='nicewoo',  # PyPI 패키지 이름
     version='0.1.0',
-    author='우성현',
-    author_email='wshyun314@naver.com',
-    description='A text processing utility package for stopword removal, keyword extraction, summarization, and language detection',
+    description='A lightweight NLP utility toolkit for stopword removal, keyword extraction, summarization, and language detection.',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
+    author='우성현',
+    author_email='wshyun314@naver.com',
     url='https://github.com/WooMongGae/nicewoo',
     project_urls={
-        'Documentation': 'https://woomonggae.github.io/nicewoo/',
+        'Documentation': 'https://woomonggae.github.io/nicewoo',
         'Source': 'https://github.com/WooMongGae/nicewoo',
         'Tracker': 'https://github.com/WooMongGae/nicewoo/issues',
     },
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+    install_requires=[
+        'nltk>=3.8',
+        'scikit-learn>=1.2',
+        'langdetect>=1.0',
+        'sumy>=0.11.0',
+        'numpy>=1.21.0',
+        'scipy>=1.7.0',
     ],
     python_requires='>=3.8',
-    install_requires=[
-        'nltk',
-        'scikit-learn',
-        'langdetect',
-        'sumy',
-        'numpy<2.0.0',
-        'scipy<1.14.0',
-        'gensim==4.3.3'
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Text Processing :: Linguistic',
     ],
+    keywords='nlp stopwords tf-idf summarization language-detection',
     include_package_data=True,
+    zip_safe=False,
 )
